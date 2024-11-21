851817cc423bd7fc2bfef474adb7f3d41cf350b8
KewanEds <kewan.edragas@edu.esiee.fr>
2024-11-21 10:34:58 +0000

    mon rendu

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
-#### Fonction secondaire
-
-
+"""Permet de tester si une chaîne de caractère est un palindrome
+"""
 def ispalindrome(p):
-
-    # votre code ici
-    
-    return False
+    """Mets la chaîne de caractère en minuscule et supprime les caractères indésirables.
+    Test ensuite l'égalité
+    """
+    p = p.lower()
+    p = p.translate(str.maketrans("éèêëàâäîïôöùûüç","eeeeaaaiioouuuc"," &_-([{)]}=+°?.,;:!¨*'"))
+    s = p[::-1]
+    return s == p
 
 #### Fonction principale
 
 
 def main():
-
-    # vos appels à la fonction secondaire ici
+    """Appelle la fonction secondaire ispalindrome et la test
+    """
+    print(ispalindrome("bonjour"))
+    print(ispalindrome("racecar"))
 
     for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
         print(s, ispalindrome(s))
